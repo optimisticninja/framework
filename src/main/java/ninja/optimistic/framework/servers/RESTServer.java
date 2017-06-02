9d1d74ccb4c1fd14032696053f6af8253d2a3c2b
Jonesy <Jonesy@DESKTOP-QEKKE98.lan>
2017-06-02 08:33:48 -0700

    Embedded jetty

--- src/main/java/ninja/optimistic/framework/servers/RESTServer.java
+++ src/main/java/ninja/optimistic/framework/servers/RESTServer.java
@@ -4,14 +4,20 @@ import java.util.logging.Logger;
 
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
 
-public abstract class RESTServer implements Server, IRESTServer {
+public abstract class RESTServer implements ninja.optimistic.framework.servers.Server, IRESTServer {
 	private static final Logger log = Logger.getLogger(RESTServer.class.getName());
 	private org.eclipse.jetty.server.Server server;
 
-	protected RESTServer(int port) {
+	protected RESTServer(int port, Class<?> api) {
 		this.server = new org.eclipse.jetty.server.Server(port);
-		
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/framework-example-server/brain");
+		server.setHandler(context);
+		ServletHolder servlet = context.addServlet(ServletContainer.class, "/*");
+		servlet.setInitOrder(0);
+		servlet.setInitParameter("jersey.config.server.provider.classnames", api.getCanonicalName());
 	}
 
 	@Override
@@ -30,5 +36,10 @@ public abstract class RESTServer implements Server, IRESTServer {
 	public void addServlet(ServletHolder servlet, String name) {
 		ServletContextHandler context = new ServletContextHandler(this.server, name + "/*");
 		context.addServlet(servlet, "/*");
+	}
+	
+	@Override
+	public void close() {
+		throw new UnsupportedOperationException();
 	}
 }
