a276b918f9d1dbe75d279cf145dd75e5acf99d75
Jonesy <Jonesy@DESKTOP-QEKKE98.lan>
2017-06-05 23:37:06 -0700

    Refactor

--- src/main/java/ninja/optimistic/framework/servers/config/RESTConfig.java
+++ src/main/java/ninja/optimistic/framework/servers/config/RESTConfig.java
@@ -2,11 +2,11 @@ package ninja.optimistic.framework.servers.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
-import ninja.optimistic.framework.providers.GsonMessageBodyHandler;
+import ninja.optimistic.framework.shims.GsonJerseyShim;
 
 public abstract class RESTConfig extends ResourceConfig {
 	public RESTConfig(String apiPackage) {
 		packages(apiPackage);
-		register(GsonMessageBodyHandler.class);
+		register(GsonJerseyShim.class);
 	}
 }
